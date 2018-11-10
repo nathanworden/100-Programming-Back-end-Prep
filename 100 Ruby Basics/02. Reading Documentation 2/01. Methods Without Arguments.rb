361a4ddaf63738d862a4b1387b3687835b5b01fd
Nathan Worden <worden.nathan@gmail.com>
2018-11-10 10:47:17 -0600

    adding files

---
+++ 100 Ruby Basics/02. Reading Documentation 2/01. Methods Without Arguments.rb
@@ -0,0 +1,12 @@
+# Methods Without Arguments
+# How would you use String#upcase to create an uppercase version of the string "xyz"?
+
+# My answer:
+
+result = "xyz".upcase
+
+p result
+
+result_downcase = result.downcase
+
+p result_downcase
