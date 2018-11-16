b62122443bd8a9555a2e9d41c67e85076d5fc97c
Nathan Worden <worden.nathan@gmail.com>
2018-11-16 06:50:48 -0600

    Modified Greet Your Friends

---
+++ 100 Ruby Basics/04. Loops 1/10. Greet Your Friends.rb
@@ -0,0 +1,15 @@
+# Greet Your Friends
+# Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+# Expected output:
+
+# Hello, Sarah!
+# Hello, John!
+# Hello, Hannah!
+# Hello, Dave!
+
+
+for i in friends 
+  puts "Hello, #{i}!"
+end
