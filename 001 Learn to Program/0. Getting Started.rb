49d3e7b4150fb011a49ce019e160e21cd5179549
Nathan Worden <worden.nathan@gmail.com>
2018-11-23 15:06:31 -0600

    Learn to Program

---
+++ 001 Learn to Program/0. Getting Started.rb
@@ -0,0 +1,28 @@
+# Getting Started
+# Chapter 0
+ 
+# When you program a computer, you have to "speak" in a language your computer understands: a programming language. There are lots and lots of different languages out there, and many of them are excellent. In this tutorial I chose to use my favorite programming language, Ruby.
+
+# Aside from being my favorite, Ruby is also the easiest programming language I have seen (and I've seen quite a few). In fact, that's the real reason I'm writing this tutorial: I didn't decide to write a tutorial, and then choose Ruby because it's my favorite; instead, I found Ruby to be so easy that I decided there really ought to be a good beginner's tutorial which uses it. It's Ruby's simplicity which prompted this tutorial, not the fact that it's my favorite. (Writing a similar tutorial using another language, like C++ or Java, would have required hundreds and hundreds of pages.) But don't think that Ruby is a beginner's language just because it is easy! It is a powerful, professional-strength programming language if ever there was one.
+
+# When you write something in a human language, what is written is called text. When you write something in a computer language, what is written is called code. I have included lots of examples of Ruby code throughout this tutorial, most of them complete programs you can run on your own computer. To make the code easier to read, I have colored parts of the code different colors. (For example, numbers are always  green.) Anything you are supposed to type in will be in a  dotted box, and anything a program prints out will be in a grey box.
+
+# If you come across something you don't understand, or you have a question which wasn't answered, write it down and keep reading! It's quite possible that the answer will come in a later chapter. However, if your question was not answered by the last chapter, I will tell you where you can go to ask it. There are lots of wonderful people out there more than willing to help; you just need to know where they are.
+
+# But first we need to download and install Ruby onto your computer.
+
+
+# Macintosh Installation
+# If you have Mac OS X 10.2 (Jaguar) or later, then you already have Ruby on your system! What could be easier?
+
+# In order to program, you need to be able to write programs and to run programs. To do this, you will need a text editor and a command line.
+
+# Your command line is accessible through the Terminal application (found in Applications/Utilities).
+
+# For a text editor, you can use whatever one you are familiar or comfortable with. My favorite text editor is Sublime Text. If you use TextEdit, however, make sure you save your programs as text-only! Otherwise your programs will not work.
+
+# And that's it! You're all set to learn to program.
+
+
+
+author = "Chris Pine"
